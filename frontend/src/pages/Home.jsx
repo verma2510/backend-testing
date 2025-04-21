f3db9a4c5830ba718199d3cd7a489e5453fa5c5b
Aman <162727802+verma2510@users.noreply.github.com>
2025-04-21 22:30:55 +0530

    updated frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div className="outline-1 outline-amber-400 flex justify-center gap-10 p-4">
+    <div className="h-screen bg-[#0b4f4bc6] flex items-center justify-center gap-10 p-4">
       <input
         type="text"
         placeholder="Your Name..."
-        className="border-1 rounded-md"
+        className="p-1 bg-white rounded-md"
       />
       <input
         type="text"
         placeholder="Your Desgination..."
-        className="border-1 rounded-md"
+        className="p-1 bg-white rounded-md"
       />
       <input
         type="text"
         placeholder="Your Age..."
-        className="border-1 rounded-md"
+        className="p-1  bg-white rounded-md"
       />
     </div>
   );
